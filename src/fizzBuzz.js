d8a5d140194ed6363632061bf71b819be6d75709
Laurenlau <harris_lau@hotmail.co.uk>
2019-08-21 23:19:25 +0100

    Fizzbuzz program with tests passing

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,22 @@
-let Fizzbuzz = function(){
+function FizzBuzz() {}
 
+var FizzBuzz = function () {} 
 
+  FizzBuzz.prototype.says = function(number) {
+    if (this._isDivisibleBy(15, number)) {
+       return "Fizzbuzz";
+    }
+    else if (this._isDivisibleBy(3, number)) {
+      return "Fizz";
+    }
+    else if (this._isDivisibleBy(5, number)) {
+      return "Buzz";
+    }
+    else {
+    return number;
+    }
+  }
 
-};
+FizzBuzz.prototype._isDivisibleBy = function(divisor, number) {
+  return number % divisor === 0;
+}

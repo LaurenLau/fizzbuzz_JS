7c32f122cdfaab05f8a492de17c9c8abbd848cb1
Laurenlau <harris_lau@hotmail.co.uk>
2019-08-21 23:18:11 +0100

    Refactor spec file

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,28 +1,40 @@
 
-describe ('Fizzbuzz', function() {
+describe ('FizzBuzz', function() {
   
-  var fizzbuzz;
+  var fizzBuzz;
 
-  beforeEach( function() {
-  fizzbuzz = new Fizzbuzz();
-  });
-  
-  it('it returns fizz for multiples of 3', function() {
-    expect(fizzbuzz(3)).toEqual(3);
-    expect(fizzbuzz(9)).toEqual(9);
+  beforeEach(function() {
+  fizzBuzz = new FizzBuzz();
   });
 
-  it('it returns fizz for multiples of 5', function() {
-    expect(fizzbuzz(5)).toEqual(5);
-    expect(fizzbuzz(5)).toEqual(5);
-  });  
-  
-  it('it returns fizz for multiples of 3 and 5', function() {
-    expect(fizzbuzz(45)).toEqual(45);
-    expect(fizzbuzz(60)).toEqual(60);
+  describe('multiples of 3', function() {
+    it('it returns fizz for multiples of 3', function() {
+      expect(fizzBuzz.says(3)).toEqual('Fizz');
+      expect(fizzBuzz.says(9)).toEqual('Fizz');
+    });
+  });
+
+  describe('multiples of 5', function() {
+    it('it returns buzz for multiples of 5', function() {
+      expect(fizzBuzz.says(5)).toEqual('Buzz');
+      expect(fizzBuzz.says(25)).toEqual('Buzz');
+    });  
+  });
+
+  describe('multiples of 3 and 5', function() {
+    it('it returns fizzbuzz for multiples of 3 and 5', function() {
+      expect(fizzBuzz.says(15)).toEqual('Fizzbuzz');
+      expect(fizzBuzz.says(75)).toEqual('Fizzbuzz');
+    });
+  });
+
+  describe('all other numbers', function() {
+    it('it returns the current number', function() {
+      expect(fizzBuzz.says(1)).toEqual(1);
+      expect(fizzBuzz.says(11)).toEqual(11);
+    });
   });
 
 });
 
-
-
+  
